4e67ac100ea560130ab99623806f9b6bcabe424f
Ubuntu <ubuntu@ip-172-31-25-61.us-west-2.compute.internal>
2013-07-14 19:35:47 +0000

    Added web.js

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+var displayText = '';
 
+displayText = fs.readFileSync('index.html', 'utf8'); 
+
+app.get('/', function(request, response) {
+  response.send(displayText);
+});
+ 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
